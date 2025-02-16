ed250e1e758400786b04a7836391879d7651353e
Firegame224 <xg474627@gmail.com>
2025-02-17 06:41:36 +0700

    first-commit

---
+++ src/libs/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseurl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabasekey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
+
+export const supabase = createClient(supabaseurl!,supabasekey!)
